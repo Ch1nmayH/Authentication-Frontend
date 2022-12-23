b4b6195b84176cbfd24378e289606bad3739a074
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-24 00:53:06 +0530

    Imported react router dom and added routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Logout from "./Components/Logout";
 
-import Register from "./Components/Register/Register";
-import Login from "./Components/Login/Login";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Home from "./Components/home";
+import NavBar from "./Components/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Register />
-    <Login />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

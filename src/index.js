9317f69db2761889270e85ff7c4d5e6e4c687ce0
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-28 17:42:01 +0530

    cleaned up the routes and added new component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import Logout from "./Components/Logout";
-
-import Register from "./Components/Register";
-import Login from "./Components/Login";
-import Home from "./Components/home";
 import NavBar from "./Components/NavBar";
+import App from "./Components/App";
+import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <NavBar />
+    <App />
+  </BrowserRouter>
 );

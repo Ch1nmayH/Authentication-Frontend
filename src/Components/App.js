b465e4586141a873db42d88d794fb1d122e9611f
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-29 00:28:25 +0530

    added useContext to store user data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,26 @@
+import { useContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { UserContext } from "../utils/CreateContext";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
+import NavBar from "./NavBar";
+import Members from "./Members";
 
 const App = () => {
+  let [user, setUser] = useState(null);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/signup" element={<Home />} />
-      </Routes>
+      <UserContext.Provider value={{ user, setUser }}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signup" element={<Home />} />
+          <Route path="/members" element={<Members />} />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 };

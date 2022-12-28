1b50e3d9033f250ebd563287a10e3688e2c4f3cc
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-29 00:28:59 +0530

    display current user using useContext

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { toast, ToastContainer } from "react-toastify";
+import { UserContext } from "../utils/CreateContext";
 
 const customId = "custom-id-yes";
 
 const Home = () => {
+  let { user } = useContext(UserContext);
   const notify = () => {
     toast("I cannot be duplicated!", {
       toastId: customId,
@@ -14,6 +16,7 @@ const Home = () => {
       <div>
         <button onClick={notify}>Notify</button>
       </div>
+      <h1>{user ? user.name : "Guest"}</h1>
       <ToastContainer />
     </>
   );

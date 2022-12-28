bf6768819c2a684e1dcf651fe3b48ca5dbd3ffdf
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-29 00:29:52 +0530

    added 1 sec timeout after logout

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,39 @@
 import Cookies from "js-cookie";
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import "../styles/NavBar.css";
+import { UserContext } from "../utils/CreateContext";
+
 const NavBar = () => {
+  let { user, setUser } = useContext(UserContext);
+  // let userExist = localStorage.getItem("user-info");
+  // if (userExist) {
+  //   setUser(JSON.parse(userExist));
+  // }
   let navigate = useNavigate();
   const logoutFunction = () => {
-    Cookies.set("token", "", { expires: 0.0000000000000001 });
-    navigate("/login");
+    try {
+      Cookies.set("token", "", { expires: 0.0000000000000001 });
+
+      toast.success("Successfully Logged out", {
+        toastId: "logout",
+        position: toast.POSITION.TOP_CENTER,
+      });
+
+      <ToastContainer />;
+      localStorage.removeItem("user-info");
+      setTimeout(() => {
+        navigate("/login");
+        setUser(null);
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error Occured, try again.", {
+        toastId: "logoutError",
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
   };
   return (
     <>
@@ -17,7 +45,7 @@ const NavBar = () => {
               Home
             </Link>
           </li>
-          {!Cookies.get("token") ? (
+          {!Cookies.get("token") && !user ? (
             <>
               <li className="nav-items">
                 <Link className="nav-link" to="login">
@@ -32,6 +60,11 @@ const NavBar = () => {
             </>
           ) : (
             <>
+              <li className="nav-items">
+                <Link className="nav-link" to="members">
+                  Members
+                </Link>
+              </li>
               <li className="nav-items">
                 <a className="nav-link" onClick={logoutFunction}>
                   Logout
